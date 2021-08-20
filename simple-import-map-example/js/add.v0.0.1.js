464f902d31908454a85ee02313583935212c6ed9
Putu Harum Bawa <lightharum.108@gmail.com>
2021-08-20 14:31:01 +0800

    simple export map

---
+++ simple-import-map-example/js/add.v0.0.1.js
@@ -0,0 +1,7 @@
+console.log('add.js version 0.0.1');
+
+function add(x, y) {
+  return x + y;
+}
+
+export default add;
---
+++ simple-import-map-example/js/app.v0.0.1.js
@@ -0,0 +1,7 @@
+import add from 'add';
+import { subtract } from 'simpleCalculator';
+
+console.log('app.js version 0.0.1');
+
+console.log('1 + 2 = ', add(1, 2));
+console.log('10 - 2 = ', subtract(10, 2));
